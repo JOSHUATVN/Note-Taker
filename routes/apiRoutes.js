fd0e4fa90a8850b443d7a2a0fddcfb5238903b5e
JOSHUATVN <80295120+JOSHUATVN@users.noreply.github.com>
2021-06-06 16:31:56 -0500

    testing

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -4,4 +4,12 @@ module.exports = function(app) {
     app.get("/api/notes/", function(req, res) {
         res.json(noteData);
     });
+    app.post("/api/notes/", function (req, res) {
+        noteData.push(req, body);
+        res.json(true);
+    })
+        app.delete("/api/notes/", function (req, res) {
+        noteData.length = 0;
+        res.json({ok: true});
+    });
 };
--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-const exoress = require("express");
+const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 3001;
 
@@ -13,5 +13,5 @@ require("./routes/apiRoutes")(app);
 require("./routes/htmlRoutes")(app);
 
 app.listen(PORT, function () {
-  console.log("App is listening on PORT:", +PORT);
+  console.log("App is listening on PORT:", + PORT);
 });
