2a9ecf912920b8c2342c91ac4409b945f3e30d2b
JOSHUATVN <80295120+JOSHUATVN@users.noreply.github.com>
2021-06-06 19:54:39 -0500

    made some code change using a diff method

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,60 +1,58 @@
-const fs = require('fs');
-module.exports = function(app) {
-  app.get('/api/notes', function(req, res) {
-    fs.readFile('./db/db.json', (err, data) => {
-      if (err) throw err;
-      dbFile = JSON.parse(data);
-      res.send(dbFile);
-    });
+const fs = require("fs");
+
+module.exports = function (app) {
+  let notes = require("./db/db.json");
+
+  app.get("api/notes", (req, res) => {
+    return res.json(notes);
   });
 
-  app.post('/api/notes', function(req, res) {
-    const usersNote = req.body;
-
-    fs.readFile('./db/db.json', (err, data) => {
-      if (err) throw err;
-      dbFile = JSON.parse(data);
-      dbFile.push(usersNote);
-      let num = 1;
-      dbFile.forEach((note, index) => {
-        note.id = num;
-        num++;
-        return dbFile;
-      });
-      console.log(dbFile);
-
-      textData = JSON.stringify(dbFile);
-
-      fs.writeFile('./db/db.json', textData, (err, data) => {
-        if (err) throw err;
-      });
-    });
-    res.send('');
-  });
-
-  app.delete('/api/notes/:id', function(req, res) {
-    // Gets id num of note to delete
-    const deleteUsersNote = req.params.id;
-    console.log(deleteUsersNote);
-
-    fs.readFile('./db/db.json', (err, data) => {
-      if (err) throw err;
-
-      // Comparing each note's id to delete note
-      dbFile = JSON.parse(data);
-      // for each function, comparing each note's id to the chosen_for_death variable
-      for (let i = 0; i < dbFile.length; i++) {
-        if (dbFile[i].id === Number(deleteUsersNote)) {
-          dbFile.splice([i], 1);
-        }
+  app.get("/api/notes/:id", (req, res) => {
+    const id = req.params.id;
+    let base;
+    notes.forEach((i) => {
+      if (id === i.id) {
+        base = i;
+        return res.json(i);
       }
-      console.log(dbFile);
-      stringData = JSON.stringify(dbFile);
-
-      fs.writeFile('./db/db.json', textData, (err, data) => {
-        if (err) throw err;
-      });
     });
-    res.status(204).send();
+    return res.json(false);
+  });
+
+  app.post("/api/notes", (req, res) => {
+    const newNote = req.body;
+    if (notes.length === 0) {
+      newNote.id = 1;
+    } else {
+      newNote.id = notes[notes.length - 1].id + 1;
+    }
+    notes.push(newNote);
+    let stringifyNotes = JSON.stringify(notes);
+    fs.writeFile("./db/db.json", stringifyNotes, function (err) {
+      if (err) {
+        return console.log(err);
+      }
+      console.log("Success");
+    });
+    res.json(true);
+  });
+
+  app.delete("/api/notes/:id", (req, res) => {
+      const id = req.params.id;
+      let base;
+      notes.forEach((i, index) => {
+          if(id === i.id) {
+              notes.splice(index, 1);
+              const copyNote = notes.slice();
+              let stringifyNotes = JSON.stringify(copyNote);
+              fs.writeFile("./db/db.json", stringifyNotes, function (err) {
+                  if(err) {
+                      return console.log(err);
+                  }
+                  console.log("Success");
+              });
+          };
+      });
+      res.json(true);
   });
 };
